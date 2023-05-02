34ce625a6146d16cbe0dbe8ed92643fb4f716441
C0A21130 <c0a2113061@edu.teu.ac.jp>
2023-05-02 20:47:52 +0900

    [update] 初期のセンサーの値の平均値を求める

--- esp32/L3GD2H/L3GD2H.ino
+++ esp32/L3GD2H/L3GD2H.ino
@@ -2,8 +2,13 @@
 
 #define SDA_PIN 21
 #define SCL_PIN 22
+#define DATA_SIZE 16
 
 const byte L3GD2H = 0x6A;
+
+float senser_data_x[DATA_SIZE], senser_data_y[DATA_SIZE];
+int head = -3;
+float ave_x, ave_y;
 
 // レジスタの値を読み取る関数(メモリアドレス)
 byte readRegister(byte address) {
@@ -24,6 +29,18 @@ void writeRegister(byte address, byte data) {
   Wire.endTransmission();
 }
 
+// 配列の平均値を求める関数(配列、配列の長さ) : 平均値
+float getAverage(float senser_data[]) {
+  float sum = 0.0;
+  
+  for (int i = 0; i < DATA_SIZE; i++) {
+    sum += senser_data[i];
+  }
+  Serial.println(sum);
+  return sum / DATA_SIZE;
+}
+
+
 void setup() {
   pinMode(SDA_PIN, INPUT_PULLUP);
   pinMode(SCL_PIN, INPUT_PULLUP);
@@ -41,21 +58,37 @@ void setup() {
 
 void loop() {
   byte l, h; // l:last, h:header
-  byte x, y, z;
+  byte xr, yr;
+  float x, y;
 
   // xの角速度の値の求める  
   l = readRegister(0x28);
   h = readRegister(0x29);
-  x = h << 8 | l;
-  Serial.print(x * 0.07);
+  xr = h << 8 | l;
+  x = (float) xr * 0.07;
+  Serial.print(x);
   Serial.print(",");
 
   // yの角速度の値の求める
   l = readRegister(0x2A);
   h = readRegister(0x2B);
-  y = h << 8 | l;
-//  Serial.print("y : ");
-  Serial.println(y * 0.07);
-  
+  yr = h << 8 | l;
+  y = (float) yr * 0.07;
+  Serial.println(y);
+
+  // DATA_SIZEの数のセンサーの値が求められたら平均値を求める
+  if (head == DATA_SIZE) { // DATA_SIZEの数のセンサーの値が求められたとき
+    ave_x = getAverage(senser_data_x);
+    ave_y = getAverage(senser_data_y);
+    head = DATA_SIZE + 1;
+  } else if(head < DATA_SIZE) {
+    head++;
+    // 最初の16個のx,yの値を記録する
+    if (head >= 0) { // まだセンサーの値が足りないときセンサーの値を配列に記録する
+      senser_data_x[head] = x;
+      senser_data_y[head] = y;
+    }
+  }
+    
   delay(1000);
 }
