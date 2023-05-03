33d97616e34ae1b505b3ad21ffb2f21927332f9e
C0A21130 <c0a2113061@edu.teu.ac.jp>
2023-05-04 02:00:09 +0900

    [update] 鍵の状態を取得するための画面を追加

---
+++ MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+
+namespace MauiApp1;
+
+public class KeyStatus
+{
+	public string status { get; set; }
+}
+
+public partial class MainPage : ContentPage
+{
+	HttpClient client = new HttpClient();
+	JsonSerializerOptions options;
+
+    int count = 0;
+    public KeyStatus keyStatus { get; private set; }
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+    private async void KeyStatusClicked(object sender, EventArgs e)
+    {
+		var status = await GetStatus();
+		KeyStatusBtn.Text = status.ToString();
+        SemanticScreenReader.Announce(KeyStatusBtn.Text);
+    }
+
+    public async Task<KeyStatus> GetStatus() 
+	{
+		keyStatus = new KeyStatus();
+		try
+		{
+			HttpResponseMessage response = await client.GetAsync("http://192.168.2.128:80");
+			string json = await response.Content.ReadAsStringAsync();
+			keyStatus = JsonSerializer.Deserialize<KeyStatus>(json, options);
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(@"\tERROR: {0}", ex.Message);
+		}
+
+		return keyStatus;
+		
+	}
+}
+
